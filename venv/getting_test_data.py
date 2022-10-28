5d581e4233d5983e6cbd9643893dc236de3c25e6
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-10-27 21:09:24 -0500

    Update getting_test_data.py

--- venv/getting_test_data.py
+++ venv/getting_test_data.py
@@ -6,52 +6,44 @@ list_of_rows = []
 list_of_cves = []
 one_minute = 600
 four_minute = 2400
+three_minute = 1800
+seven_minute = 4200
 test_models_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/test_models/'
 #df = pd.DataFrame()
 df2 = pd.DataFrame()
+list_of_values = [1,2,3,4]
 for line in Lines:
 
     content = line.strip()
     #print(content)
-    path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content +'/' + content + '-1_freqvector_full.csv'
-    #print(path)
-    list_of_cves.append(path)
+    for k in list_of_values:
+        path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content +'/' + content + '-' + str(k) +'_freqvector_full.csv'
+        print(path)
+        list_of_cves.append(path)
 
 
-for cve_path in list_of_cves:
-    file2 = open(cve_path, 'r')
-    lines2 = file2.readlines()
-    cve = cve_path.split('/')[7]
-    #print(cve)
-    #break
+    for cve_path in list_of_cves:
+        file2 = open(cve_path, 'r')
+        lines2 = file2.readlines()
+        cve = cve_path.split('/')[7]
+        #print(cve)
+        #break
 
-    line_number =1
-    for line in lines2:
-        content = line.strip()
+        line_number =1
+        for line in lines2:
+            content = line.strip()
 
-        if line_number == 1:
-            line_number +=1
-            #print(content)
-            continue
+            if line_number == 1:
+                line_number +=1
+                continue
 
-        elif line_number > four_minute+1:
-            #print(cve_path,content)
-            data = content.split(",")[1:]
+            elif line_number > three_minute+1 and line_number <= seven_minute+1:
+                #print(cve_path,content)
+                data = content.split(",")[1:]
 
-
-            #print(data,content)
-            #print(type(data))
-            list_of_rows.append(data)
-            #df = pd.DataFrame(data)
-            #df2.append(df)
-            #break
-        line_number +=1
+                list_of_rows.append(data)
+            line_number += 1
     df = pd.DataFrame(list_of_rows)
     list_of_rows.clear()
     path_to_pickle = test_models_path + cve + '.pkl'
     df.to_pickle(path_to_pickle)
-
-
-#print("NEW")
-#df = pd.DataFrame(list_of_rows)
-#print(df)

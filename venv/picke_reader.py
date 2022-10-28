30e5c4837a7ef92b0ac0f9c2a7f734affc2a787e
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-10-27 21:09:46 -0500

    Update picke_reader.py

--- venv/picke_reader.py
+++ venv/picke_reader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_classifier/CVE-2012-1823.pkl")
+#unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_classifier/CVE-2012-1823.pkl")
 #unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2012-1823.pkl")
+#unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2012-1823.pkl")
+unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/test_models/CVE-2012-1823.pkl")
 print(unpickled_df)

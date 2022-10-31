f7bee45474c06bcedacb5add5ee943cf4617e2e4
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-10-31 02:35:11 -0500

    Update getting_test_data.py

--- venv/getting_test_data.py
+++ venv/getting_test_data.py
@@ -5,10 +5,10 @@ Lines = file1.readlines()
 list_of_rows = []
 list_of_cves = []
 one_minute = 600
-four_minute = 2400
 three_minute = 1800
-seven_minute = 4200
-test_models_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/test_models/'
+four_minute = 2400
+#train_autoencoder_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/train_autoencoder/'
+train_autoencoder_path = 'C:/Users/sadun/PycharmProjects/cdl_remade/venv/train_autoencoder/'
 #df = pd.DataFrame()
 df2 = pd.DataFrame()
 list_of_values = [1,2,3,4]
@@ -17,33 +17,45 @@ for line in Lines:
     content = line.strip()
     #print(content)
     for k in list_of_values:
-        path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content +'/' + content + '-' + str(k) +'_freqvector_full.csv'
+        #path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content +'/' + content + '-' + str(k) +'_freqvector_full.csv'
+        path = 'C:/Users/sadun/PycharmProjects/cdl_remade/venv/data-CDL/' + content + '/' + content + '-' + str(
+            k) + '_freqvector_full.csv'
         print(path)
         list_of_cves.append(path)
 
+cve_four_counter=1
+for cve_path in list_of_cves:
+    file2 = open(cve_path, 'r')
+    lines2 = file2.readlines()
+    cve = cve_path.split('/')[7]
+    #print(cve)
+    #break
 
-    for cve_path in list_of_cves:
-        file2 = open(cve_path, 'r')
-        lines2 = file2.readlines()
-        cve = cve_path.split('/')[7]
-        #print(cve)
+    line_number =1
+    for l2 in lines2:
+        content = l2.strip()
+
+        if line_number == 1:
+            line_number +=1
+            continue
+
+        elif line_number > one_minute + 1 and line_number <= three_minute+1:
+            #print(cve_path,content)
+            data = content.split(",")[1:]
+
+            list_of_rows.append(data)
+        line_number += 1
+
+    if cve_four_counter % 4 ==0:
+
+        df = pd.DataFrame(list_of_rows)
+
+        list_of_rows.clear()
+        path_to_pickle = train_autoencoder_path + cve + '.pkl'
+        #print(df)
         #break
+        df.to_pickle(path_to_pickle)
 
-        line_number =1
-        for line in lines2:
-            content = line.strip()
+    cve_four_counter +=1
 
-            if line_number == 1:
-                line_number +=1
-                continue
 
-            elif line_number > three_minute+1 and line_number <= seven_minute+1:
-                #print(cve_path,content)
-                data = content.split(",")[1:]
-
-                list_of_rows.append(data)
-            line_number += 1
-    df = pd.DataFrame(list_of_rows)
-    list_of_rows.clear()
-    path_to_pickle = test_models_path + cve + '.pkl'
-    df.to_pickle(path_to_pickle)

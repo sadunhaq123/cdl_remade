46199cab9cfcc8e6c78f734a12d596bfc6728407
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-10-31 02:34:34 -0500

    Update getting_application_classifier_training_data.py

--- venv/getting_application_classifier_training_data.py
+++ venv/getting_application_classifier_training_data.py
@@ -6,7 +6,8 @@ list_of_rows = []
 list_of_cves = []
 one_minute = 600
 four_minute = 2400
-train_classifier_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/train_classifier/'
+#train_classifier_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/train_classifier/'
+train_classifier_path = 'C:/Users/sadun/PycharmProjects/cdl_remade/venv/train_classifier/'
 #df = pd.DataFrame()
 df2 = pd.DataFrame()
 list_of_values = [1,2,3,4]
@@ -15,33 +16,45 @@ for line in Lines:
     content = line.strip()
     #print(content)
     for k in list_of_values:
-        path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content +'/' + content + '-' + str(k) +'_freqvector_full.csv'
+        #path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content +'/' + content + '-' + str(k) +'_freqvector_full.csv'
+        path = 'C:/Users/sadun/PycharmProjects/cdl_remade/venv/data-CDL/' + content + '/' + content + '-' + str(
+            k) + '_freqvector_full.csv'
         print(path)
         list_of_cves.append(path)
 
 
-    for cve_path in list_of_cves:
-        file2 = open(cve_path, 'r')
-        lines2 = file2.readlines()
-        cve = cve_path.split('/')[7]
-        #print(cve)
+cve_four_counter=1
+for cve_path in list_of_cves:
+    file2 = open(cve_path, 'r')
+    lines2 = file2.readlines()
+    cve = cve_path.split('/')[7]
+    #print(cve)
+    #break
+
+    line_number =1
+    for l2 in lines2:
+        content = l2.strip()
+
+        if line_number == 1:
+            line_number +=1
+            continue
+
+
+        elif line_number <= one_minute + 1:
+            #print(cve_path,content)
+            data = content.split(",")[1:]
+
+            list_of_rows.append(data)
+        line_number += 1
+
+    if cve_four_counter % 4 ==0:
+
+        df = pd.DataFrame(list_of_rows)
+
+        list_of_rows.clear()
+        path_to_pickle = train_classifier_path + cve + '.pkl'
+        #print(df)
         #break
+        df.to_pickle(path_to_pickle)
 
-        line_number =1
-        for line in lines2:
-            content = line.strip()
-
-            if line_number == 1:
-                line_number +=1
-                continue
-
-            elif line_number <= one_minute+1:
-                #print(cve_path,content)
-                data = content.split(",")[1:]
-
-                list_of_rows.append(data)
-            line_number += 1
-    df = pd.DataFrame(list_of_rows)
-    list_of_rows.clear()
-    path_to_pickle = train_classifier_path + cve + '.pkl'
-    df.to_pickle(path_to_pickle)
+    cve_four_counter +=1

52cff61656c4c68cdba32a0c147d33e1458211a8
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-11-09 16:06:43 -0600

    Add files via upload

--- venv/train_application_classifier.py
+++ venv/train_application_classifier.py
@@ -1,28 +1,42 @@
 import pandas as pd
 import os
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn import metrics
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import preprocessing
+
+train_data_pickle = ['train_classifier/all_combined.pkl']
+test_data_pickle = ['test_classifier/all_combined.pkl']
+
+train_data = pd.read_pickle(train_data_pickle[0])
+test_data = pd.read_pickle(test_data_pickle[0])
+
+train_data_x = train_data.iloc[:, :-1]
+train_data_y = train_data.iloc[:, -1:]
+#print(train_data_y)
+
+test_data_x = test_data.iloc[:, :-1]
+test_data_y = test_data.iloc[:, -1:]
+#print(test_data_y)
+
+sc = StandardScaler()
+scaled_train_data_x = sc.fit_transform(train_data_x)
+scaled_test_data_x = sc.fit_transform(test_data_x)
+
+label_encoder = preprocessing.LabelEncoder()
+label_encoded_train_y = label_encoder.fit_transform(train_data_y)
+label_encoded_test_y = label_encoder.fit_transform(test_data_y)
+
+clf = RandomForestClassifier(n_estimators = 200)
+
+clf.fit(scaled_train_data_x, label_encoded_train_y)
+y_pred = clf.predict(scaled_test_data_x)
+accuracy = metrics.accuracy_score(y_pred, label_encoded_test_y)
+
+print(accuracy)
 
 
-cve_and_application_mapping = {}
 
-file1 = open('cve_and_application.txt', 'r')
-Lines = file1.readlines()
-
-for line in Lines:
-    content = line.strip()
-    content = content.split('/')
-    #print(content)
-    cve_and_application_mapping.setdefault(content[0], [])
-    cve_and_application_mapping[content[0]].append(content[1])
-    #if content[0] not in cve_and_application_mapping.keys():
-    #    cve_and_application_mapping[content[0]] = content[1]
-    #else:
-    #    cve_and_application_mapping[content[0]].append(content[1])
-
-c=0
-for i, j in cve_and_application_mapping.items():
-    print(i, j)
-    c1 = len(j)
-    c +=c1
-
-print(c)

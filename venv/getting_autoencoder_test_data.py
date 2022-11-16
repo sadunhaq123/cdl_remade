7026d022f7d803c4f05644d1eefe35f3d5d6f3cc
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-11-16 17:47:50 -0600

    Add files via upload

--- venv/getting_autoencoder_test_data.py
+++ venv/getting_autoencoder_test_data.py
@@ -7,11 +7,14 @@ list_of_cves = []
 one_minute = 600
 three_minute = 1800
 four_minute = 2400
-train_autoencoder_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/train_autoencoder/'
+seven_minute = 4200
+test_autoencoder_path = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/test_autoencoder/'
 #train_autoencoder_path = 'C:/Users/sadun/PycharmProjects/cdl_remade/venv/train_autoencoder/'
 #df = pd.DataFrame()
 df2 = pd.DataFrame()
 list_of_values = [1,2,3,4]
+list_of_path_timings = []
+list_of_timings = []
 normal = 1
 anomaly = 0
 list_of_all = []
@@ -25,13 +28,35 @@ for line in Lines:
         print(path)
         list_of_cves.append(path)
 
+    path_timing = 'C:/Users/12103/PycharmProjects/cdl_remade/venv/data-CDL/' + content + '/timing.csv'
+    list_of_path_timings.append(path_timing)
+
+for paths in list_of_path_timings:
+    file3 = open(paths, 'r')
+    lines3 = file3.readlines()
+    line_number_3=1
+    for lines in lines3:
+        content = lines.strip()
+        print(content)
+        if line_number_3 > 1:
+            split_content = content.split(',')
+            for i in range(len(split_content)):
+                if i >0:
+                    list_of_timings.append(split_content[i])
+        line_number_3 += 1
+
+
 cve_four_counter=1
 for cve_path in list_of_cves:
     file2 = open(cve_path, 'r')
     lines2 = file2.readlines()
     cve = cve_path.split('/')[7]
-    #print(cve)
     #break
+    begin = int(list_of_timings.pop(0))
+    end = int(list_of_timings.pop(0))
+    #print(cve)
+    #print(begin)
+    #print(end)
 
     line_number =1
     for l2 in lines2:
@@ -41,11 +66,15 @@ for cve_path in list_of_cves:
             line_number +=1
             continue
 
-        elif line_number > one_minute + 1 and line_number <= three_minute+1:
+        elif line_number > three_minute + 1 and line_number <= seven_minute+1:
             #print(cve_path,content)
             data = content.split(",")[1:]
-            data.append(normal)
-            #print(data)
+            #print(cve)
+            if line_number >= begin and line_number <= end:
+                data.append(anomaly)
+                #print("ANOMALY")
+            else:
+                data.append(normal)
 
             list_of_rows.append(data)
             list_of_all.append(data)
@@ -56,7 +85,7 @@ for cve_path in list_of_cves:
         df = pd.DataFrame(list_of_rows)
 
         list_of_rows.clear()
-        path_to_pickle = train_autoencoder_path + cve + '.pkl'
+        path_to_pickle = test_autoencoder_path + cve + '.pkl'
         #print(df)
         #break
         df.to_pickle(path_to_pickle)
@@ -64,7 +93,4 @@ for cve_path in list_of_cves:
     cve_four_counter +=1
 
 df3 = pd.DataFrame(list_of_all)
-df3.to_pickle(train_autoencoder_path + 'train_autoencoder_all.pkl')
-
-
-
+df3.to_pickle(test_autoencoder_path + 'train_autoencoder_all.pkl')
--- venv/picke_reader.py
+++ venv/picke_reader.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 #unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_classifier/CVE-2012-1823.pkl")
+#unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_classifier/all_combined.pkl")
 #unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2012-1823.pkl")
 #unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2012-1823.pkl")
-#unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/test_models/CVE-2012-1823.pkl")
+unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/test_autoencoder/CVE-2012-1823.pkl")
 #unpickled_df = pd.read_pickle("C:/Users/sadun/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2012-1823.pkl")
-unpickled_df = pd.read_pickle("C:/Users/sadun/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2014-0050.pkl")
-print(unpickled_df)
+#unpickled_df = pd.read_pickle("C:/Users/sadun/PycharmProjects/cdl_remade/venv/train_autoencoder/CVE-2014-0050.pkl")
+#unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/test_classifier/CVE-2012-1823.pkl")
+#unpickled_df = pd.read_pickle("C:/Users/12103/PycharmProjects/cdl_remade/venv/test_classifier/all_combined.pkl")
+#unpickled_df = pd.read_pickle("C:/Users/12103/Downloads/SHIL-master/SHIL-master/data/classifier/randomforest-200.pkl")
+print(unpickled_df[598:610])

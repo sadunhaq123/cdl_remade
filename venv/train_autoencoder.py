4820edab5f972d624f665d67f0635dd124ab9686
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2022-11-16 19:19:43 -0600

    Add files via upload

--- venv/train_autoencoder.py
+++ venv/train_autoencoder.py
@@ -9,29 +9,46 @@ import matplotlib.pyplot as plt
 import os
 import tensorflow as tf
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 file1 = open('train_autoencoder_pickle.txt', 'r')
 
 #Lines = file1.readlines()
-Lines = ['train_autoencoder/all_combined_train_auto_encoder.pkl']
-Line2 = ['test_models/all_combined_test_auto_encoder.pkl']
+Lines = ['train_autoencoder/train_autoencoder_all.pkl']
+Line2 = ['test_autoencoder/test_autoencoder_all.pkl']
 scaler = StandardScaler()
 norm = MinMaxScaler()
 for line in Lines:
     content = line.strip()
     train_data = pd.read_pickle(content)
+    #train_data= train_data.values
+    print(type(train_data))
     print(content, train_data.shape)
     rows, columns = train_data.shape
     print(rows, columns)
-    dataset_train = norm.fit_transform(train_data)
+    train_x = train_data.iloc[:, 0:-1]
+    #print(train_x)
+    train_labels = train_data.iloc[:, -1]
+    print(train_x.shape)
+    #min_val = tf.reduce_min(train_x)
+    #max_val = tf.reduce_max(train_x)
+    #dataset_train = (train_x - min_val) / (max_val - min_val)
+    #dataset_train = tf.cast(dataset_train, tf.float32)
+    dataset_train = norm.fit_transform(train_x)
+    #dataset_labels = norm.fit_transform(train_labels)
     #print(dataset_train)
     print(dataset_train.shape)
     #break
 
 test_data = pd.read_pickle(Line2[0])
-dataset_test = norm.fit_transform(test_data)
+#test_data = test_data.values
+test_x = test_data.iloc[:, 0:-1]
+test_labels = test_data.iloc[:, -1]
+#dataset_test = (test_x - min_val) / (max_val - min_val)
+#dataset_test = tf.cast(dataset_test, tf.float32)
+dataset_test = norm.fit_transform(test_x)
+#dataset_labels = norm.fit_transform(test_labels)
 #dataset_test = dataset_test[:10000]
 #dataset_train = dataset_train[:10000]
 print(dataset_test.shape)
@@ -64,34 +81,15 @@ class AutoEncoder(Model):
         decoded = self.decoder(encoded)
         return decoded
 
-def encoder():
-    print("IN ENC")
-    encoder_input = keras.Input(shape=(8791200), name ='enc')
-    #encoder_input = keras.Input(shape=(1500, 555), name ='enc')
-    #encoder_flatten = keras.layers.Flatten()(encoder_input)
-    #encoder_layer1 = keras.layers.Dense(36630, activation='sigmoid')(encoder_flatten)
-    encoder_layer2 = keras.layers.Dense(2400, activation='sigmoid')(encoder_input)
-    encoder_output = keras.layers.Dense(64, activation='sigmoid')(encoder_layer2)
-
-#encoder()
-print("HAPPY")
-#encoder = keras.Model(encoder_input, encoder_output, name='encoder')
-
-def decoder():
-    decoder_input = keras.layers.Dense(64, activation="sigmoid")(encoder_output)
-    decoder_layer1 = keras.layers.Dense(2400, activation='sigmoid')(decoder_input)
-    #decoder_layer2 = keras.layers.Dense(36630, activation='sigmoid')(decoder_layer1)
-    decoder_layer3 = keras.layers.Dense(8791200, activation='sigmoid')(decoder_layer1)
-    #decoder_output = keras.Layers.Reshape((158400, 555))(decoder_layer3)
 
 
 model = AutoEncoder()
-model.compile(loss='msle', metrics=['mse'], optimizer='RMSprop')
+model.compile(loss='mse', metrics=['mse'], optimizer='RMSprop')
 
 history = model.fit(
     dataset_train,
     dataset_train,
-    epochs=20,
+    epochs=10,
     batch_size=32,
     validation_data=(dataset_test, dataset_test)
 )
@@ -104,20 +102,32 @@ history = model.fit(
 def find_threshold(model, x_train_scaled):
   reconstructions = model.predict(x_train_scaled)
   # provides losses of individual instances
-  reconstruction_errors = tf.keras.losses.msle(reconstructions, x_train_scaled)
+  reconstruction_errors = tf.keras.losses.mse(reconstructions, x_train_scaled)
   # threshold for anomaly scores
   threshold = np.mean(reconstruction_errors.numpy()) \
       + np.std(reconstruction_errors.numpy())
+  #threshold = 0.1
+  plt.hist(reconstruction_errors[None, :], bins=20)
+  plt.xlabel("Train loss")
+  plt.ylabel("No of examples")
+  plt.show()
+
   return threshold
 
 def get_predictions(model, x_test_scaled, threshold):
   predictions = model.predict(x_test_scaled)
   # provides losses of individual instances
-  errors = tf.keras.losses.msle(predictions, x_test_scaled)
+  errors = tf.keras.losses.mse(predictions, x_test_scaled)
+
+  plt.hist(errors[None, :], bins=20)
+  plt.xlabel("Test loss")
+  plt.ylabel("No of examples")
+  plt.show()
   # 0 = anomaly, 1 = normal
   #anomaly_mask = pd.Series(errors) > threshold
   #preds = anomaly_mask.map(lambda x: 0.0 if x == True else 1.0)
-  return predictions
+  return tf.math.less(errors, threshold)
+  #return predictions
 
 threshold = find_threshold(model, dataset_train)
 print(f"Threshold: {threshold}")
@@ -127,11 +137,16 @@ print(predictions.shape)
 print(dataset_test.shape)
 print(predictions)
 print(dataset_test)
-y_pred=np.argmax(predictions, axis=1)
-y_test=np.argmax(dataset_test, axis=1)
-print(y_pred)
-print(y_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-print(accuracy_score(y_test, y_pred))
+#y_pred=predictions
+#y_test=dataset_test
+#y_pred=np.argmax(predictions, axis=1)
+#y_test=np.argmax(dataset_test, axis=1)
+#print(y_pred)
+#print(y_test)
+#cm = confusion_matrix(y_test, y_pred)
+#print(cm)
+print("Accuracy :", accuracy_score(test_labels, predictions))
+#print("Accuracy :", accuracy_score(y_test, y_pred))
+#print("Precision:", precision_score(y_test, y_pred))
+#print("Recall   :", recall_score(y_test, y_pred))
 # 0.944
